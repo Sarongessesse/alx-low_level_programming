fdda1bf76df6772a081fecbdb4a3926635b20c54
saron <sarongessesse@gmail.com>
2021-10-21 05:44:16 -0700

    ifelse

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/** 
-* main - determine if number is positive or negative
+/** main - determine if number is positive or negative
 *
 *
 * Return:0
 */
-
 int main(void)
 {
 int n;
