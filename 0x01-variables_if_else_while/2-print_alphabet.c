f855fff109d9385a3e6c8a3c67d8645b5fa83d5b
saron <sarongessesse@gmail.com>
2021-11-04 01:48:06 -0700

    print alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - determine 
+*
+*
+* return: 0
+*/
+int main(void)
+{
+char i;
+for( i='a'; i <= 'z';i++ )
+putchar("%c\n",i);
+return (0);
+}
