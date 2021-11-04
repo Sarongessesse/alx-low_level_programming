ed7ed331a7c7e5ae43ca423cbacde72adb869042
saron <sarongessesse@gmail.com>
2021-11-04 03:03:10 -0700

    ll

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,10 +10,14 @@
 int main(void)
 {
 char i;
-for (i ='a'; i <= 'z'; i++)
+for (i = 'a'; i <= 'z'; i++)
+{
 putchar (i);
+}
 for (i = 'A'; i <= 'z'; i++)
+{
 putchar (i);
+}
 putchar ('\n');
 return (0);
 }
