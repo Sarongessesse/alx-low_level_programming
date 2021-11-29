66df8376c72e8f10303d193ac3418b0878b0b8ed
saron <sarongessesse@gmail.com>
2021-11-29 04:36:44 -0800

    new

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,14 @@ void print_alphabet_x10(void)
 {
   char x;
   for (x = 'a'; x <= 'z'; x++)
-    {
+    {{
       int y;
       while( y = 10 )
 	{
        putchar (x);
 	  y++;
 	}	
+      }
       putchar ('\n');
       return;
     }
