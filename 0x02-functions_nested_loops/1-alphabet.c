48015a421ed2d2215c35bc93e78a1eae20bbc99d
saron <sarongessesse@gmail.com>
2021-11-29 00:35:33 -0800

    n

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,21 @@
 #include "main.h"
-#include "main.c"
 /**
 *main- cehck code
 *
 *Return: 0
 */
-
+void print_alphabet(void);
+int main(void)
+{
+  print_alphabet();
+  return (0);
+}
+void print_alphabet()
+{
+  char x;
+  for ( x = 'a'; x <= 'z'; x++)
+    { putchar (x);
+      putchar ('\n');
+      return;
+    }
+}  
