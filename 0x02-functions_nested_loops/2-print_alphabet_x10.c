20529c9406a6ab4f315b78231c60ff9580df5d75
saron <sarongessesse@gmail.com>
2021-12-01 05:01:20 -0800

    l

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*void print_alphabet_x10 - alpha
+*print_alphabet_x10 - alpha
 *
 *Return:0
 */
@@ -12,8 +12,8 @@ for (x = 1; x <= 10; x++)
 char y;
 for (y = 'a'; y <= 'z'; y++ )
 {
-putchar (x); 
-}	
+putchar (x);
+}
 putchar ('\n');
 return;
 }
