e951b986fc8f097320f33aaed687d20971c816ed
saron <sarongessesse@gmail.com>
2021-12-01 05:52:57 -0800

    k

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*_islower - checks
+*
+*Return:
+*/
+int _islower( )
+{
+  char x;
+ 
+  for (x = 'a'; x <= 'z'; x++)
+    {
+      return (1);
+    }
+  for (x = 'A'; x <= 'Z'; x++)
+    {return (0);}
+
+
+
+
+
+
+
+}
